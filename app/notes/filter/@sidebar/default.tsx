a7f6b07fc4f6d01469937dbd781ca05b6c94ab89
Aleksei <bylexatime@gmail.com>
2025-08-28 12:17:17 +0300

    fix Sidebar

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,5 +1,32 @@
-import SidebarNotes from './SidebarNotes';
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import css from './SidebarNotes.module.css';
+
+const TAGS = ['All', 'Work', 'Personal', 'Meeting', 'Shopping', 'Todo'];
 
 export default function SidebarPage() {
-  return <SidebarNotes />;
+  const pathname = usePathname();
+  const activeTag = pathname.split('/').pop() || 'All';
+
+  return (
+    <ul className={css.menuList}>
+      {TAGS.map(tag => {
+        const href = `/notes/filter/${tag}`;
+        const isActive = activeTag === tag;
+
+        return (
+          <li key={tag} className={css.menuItem}>
+            <Link
+              href={href}
+              className={`${css.menuLink} ${isActive ? css.active : ''}`}
+            >
+              {tag}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
