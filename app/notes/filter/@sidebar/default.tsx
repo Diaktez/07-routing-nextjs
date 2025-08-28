2db5e26f8189b58a18ba74927cdf29213482e4ce
Aleksei <bylexatime@gmail.com>
2025-08-28 15:06:02 +0300

    change sidebar to server component, without active tags

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,27 +1,48 @@
-'use client';
+// 'use client';
 
+// import Link from 'next/link';
+// import { usePathname } from 'next/navigation';
+// import css from './SidebarNotes.module.css';
+
+// const TAGS = ['All', 'Work', 'Personal', 'Meeting', 'Shopping', 'Todo'];
+
+// export default function SidebarPage() {
+//   const pathname = usePathname();
+//   const activeTag = pathname.split('/').pop() || 'All';
+
+//   return (
+//     <ul className={css.menuList}>
+//       {TAGS.map(tag => {
+//         const href = `/notes/filter/${tag}`;
+//         const isActive = activeTag === tag;
+
+//         return (
+//           <li key={tag} className={css.menuItem}>
+//             <Link
+//               href={href}
+//               className={`${css.menuLink} ${isActive ? css.active : ''}`}
+//             >
+//               {tag}
+//             </Link>
+//           </li>
+//         );
+//       })}
+//     </ul>
+//   );
+// }
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import css from './SidebarNotes.module.css';
 
 const TAGS = ['All', 'Work', 'Personal', 'Meeting', 'Shopping', 'Todo'];
 
 export default function SidebarPage() {
-  const pathname = usePathname();
-  const activeTag = pathname.split('/').pop() || 'All';
-
   return (
     <ul className={css.menuList}>
       {TAGS.map(tag => {
         const href = `/notes/filter/${tag}`;
-        const isActive = activeTag === tag;
-
         return (
           <li key={tag} className={css.menuItem}>
-            <Link
-              href={href}
-              className={`${css.menuLink} ${isActive ? css.active : ''}`}
-            >
+            <Link href={href} className={css.menuLink}>
               {tag}
             </Link>
           </li>
